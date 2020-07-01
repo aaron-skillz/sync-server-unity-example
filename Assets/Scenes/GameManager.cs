2ac0dbcc2e1252f8ff16bc458cecaf1401458e5e
Aaron Jones <ajones@skillz.com>
2020-06-30 18:47:47 -0700

    initial example more to come

---
+++ Assets/Scenes/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SkillzSDK;
+using UnityEngine;
+
+public class GameManager : SkillzMatchDelegate
+{
+    public void OnMatchWillBegin(Match matchInfo)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnSkillzWillExit()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scenes/NakamaClient.cs
+++ Assets/Scenes/NakamaClient.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System;
+using SkillzSDK;
 using UnityEngine;
 using Nakama;
 
@@ -8,7 +9,9 @@ public class NakamaClient : MonoBehaviour
 {
     private string deviceId;
 
-    private readonly IClient client = new Client("http", "192.168.1.139", 7350, "defaultkey");
+    private readonly IClient client = new Client("http", "192.168.1.121", 7350, "defaultkey");
+
+    private string serverMatchId;
 
     private ISocket socket;
     // Start is called before the first frame update
@@ -16,7 +19,7 @@ public class NakamaClient : MonoBehaviour
     {
         deviceId = SystemInfo.deviceUniqueIdentifier;
 
-        var session = await client.AuthenticateDeviceAsync(deviceId, "test-client", false);
+        var session = await client.AuthenticateDeviceAsync(deviceId, "test-client-2", false);
         Debug.Log("Device authenticated with token:");
         Debug.Log(session.AuthToken); // raw JWT token
         Debug.LogFormat("Session user id: '{0}'", session.UserId);
@@ -28,23 +31,51 @@ public class NakamaClient : MonoBehaviour
 
         socket.Connected += () => Debug.Log("Socket Connected");
         socket.Closed += () => Debug.Log("Socket Closed");
+        socket.ReceivedMatchState += ReceiveMatchStateMessage;
 
         await socket.ConnectAsync(session);
 
 
-        var match = await socket.CreateMatchAsync();
-        Debug.LogFormat("New match with id '{0}'.", match.Id);
+
+        //var match = await socket.CreateMatchAsync();
+        //Debug.LogFormat("New match with id '{0}'.", match.Id);
+        //var matchId = "01234567890123456789.nakama";
+
+        var resp = await socket.RpcAsync("create_skillz_match", "{\"match_id\": \"01234567890123456789.nakama\"}");
+        GameMsg gMsg = JsonUtility.FromJson<GameMsg>(resp.Payload);
+        Debug.LogFormat("New Match ID: {0}", gMsg.match_id);
+
+        serverMatchId = gMsg.match_id;
+        var match = await socket.JoinMatchAsync(serverMatchId);
+
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //SkillzCrossPlatform.LaunchSkillz(new GameManager());
+    }
+
+    private void ReceiveMatchStateMessage(IMatchState matchState)
+    {
+        string messageJson = System.Text.Encoding.UTF8.GetString(matchState.State);
+        Debug.LogFormat("Message: {0}", messageJson);
+    }
+
+    public void SendMessageToServer()
+    {
+        var opCode = 1;
+        socket.SendMatchStateAsync(serverMatchId, opCode, "{\"hello\":\"from client 1\"}");
     }
 
     private void OnApplicationQuit()
     {
         socket?.CloseAsync();
     }
+}
+
+public class GameMsg
+{
+    public string match_id;
 }
